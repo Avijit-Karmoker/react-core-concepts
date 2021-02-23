02c129b2b2ba69ede73eec05ce0e721800cccefe
Coffee-Ovi <ovijit1hsc@gmail.com>
2021-02-23 12:39:47 +0600

    something learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
+  // const style = {
+  //   color: "red",
+  //   backgroundColor: 'cyan'
+  // }
+  // const person1={
+  //   name: "Ovijit karmoker",
+  //   job: "Student"
+  // }
+  // const person2 = {
+  //   name: "Sharmin Shama",
+  //   job: "Data Entry"
+  // }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,9 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        {/* <h2 style = {style}>Name: {person1.name+" "+person1.job}</h2>
+        <h2 style={{color: "pink", backgroundColor: "red"}}>Name: {person2.name+" "+person2.job}</h2> */}
+        <p>My first react app</p>
         <a
           className="App-link"
           href="https://reactjs.org"
