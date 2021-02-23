11ed2c6f8de45f230cca2f74a9d92f1d9ab6356e
Coffee-Ovi <ovijit1hsc@gmail.com>
2021-02-23 20:06:24 +0600

    module 32-8 done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
   //   name: "Sharmin Shama",
   //   job: "Data Entry"
   // }
+  const products = [
+    {name: 'Photoshop', price: "$99.99"},
+    {name: 'Illustrator', price: "$99.99"},
+    {name: 'PDF Reader', price: "$99.99"},
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +27,13 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        {/* <Products name = {products[0].name} price = {products[0].price}></Products> */}
+        {/* <Products product = {products[1]}></Products> */}
+        <Products product = {products[2]}></Products>
+        <Person name = "Ovijit" job = "Student"></Person>
+        <Person name = "Shama" job = "Data Entry"></Person>
+        <Person name = {name[0]} job = {job[0]}></Person>
+        <Person name = {name[2]} job = {job[2]}></Person>
         {/* <h2 style = {style}>Name: {person1.name+" "+person1.job}</h2>
         <h2 style={{color: "pink", backgroundColor: "red"}}>Name: {person2.name+" "+person2.job}</h2> */}
         <p>My first react app</p>
@@ -36,6 +48,46 @@ function App() {
       </header>
     </div>
   );
+}
+function Products(props){
+  const style = {
+    border: "5px solid red",
+    borderRadius: "10px",
+    padding: "30px",
+    margin: '10px',
+    height: '300px',
+    width: '300px'
+  }
+  const {name, price} = props.product;
+  return(
+    <div style = {style}>
+      {/* <h3>{props.name}</h3>
+      <h1 style = {{color: 'red'}}>{props.price}</h1>  1st way */}
+
+      {/* <h3>{props.product.name}</h3>
+      <h1 style = {{color: 'red'}}>{props.product.price}</h1> 2nd way */}
+
+      <h3>{name}</h3>
+      <h1 style = {{color: 'red'}}>{price}</h1> {/* 3rd way */}
+    </div>
+  )
+}
+const name = ['Himel', 'Tinni', 'Rezaul'];
+const job = ['Student', 'Graphics Designer', 'Gamer']
+function Person(props){
+  const style = {
+    color: '#fff',
+    backgroundColor: 'green',
+    border: "5px solid orange",
+    borderRadius: "10px",
+    padding: "30px",
+    margin: '10px'
+  }
+  return(
+    <div style = {style}>
+      <h1>Name: {props.name}</h1>
+      <h4>Job: {props.job}</h4>
+    </div>)
 }
 
 export default App;
